7f92f1287ec25fa1b280b34740ad192dfd9ab329
maadi <mishraadi110@gmail.com>
2023-09-12 00:38:52 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import { logo } from '../assets';
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt='sumz_logo'
+        className='w-28 object-contain'/>
+        <button type='button' 
+         onClick={() => window.open('https://github.com')}
+        className='black_btn'>
+          GitHub
+        </button>
+      </nav>
+      
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md: hidden'/>
+        <span className='orange_gradient'>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Exercitationem est repudiandae perferendis provident 
+        distinctio possimus! Debitis expedita ut repellat, 
+        repellendus amet atque assumenda nisi alias aspernatur
+      </h2>
+    </header>
+  )
+}
+
+export default Hero
